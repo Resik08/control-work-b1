306edda8d4531356401c2b862b4d2c7b8f206b36
Viktor <vitiati02@gmail.com>
2023-09-20 12:32:37 +0300

    task completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] LeadArray(string text)
+{
+    Console.WriteLine(text);
+    return Console.ReadLine()!.Split();
+}
+void PrintArray(string[] array, string text) {
+    Console.WriteLine($"{text}: [{string.Join(", ", array)}]");
+}
+
+int Counter(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) count++;
+    }
+    return count;
+}
+string[] NewArray(string[] arr, int count)
+{
+    string[] resultArray = new string[count];
+    int index = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            resultArray[index] = arr[i];
+            index++;
+        }
+    }
+    return resultArray;
+}
+
+string[] array = LeadArray("Введите несколько слов, цифр или символов, разделяя их пробелами");
+PrintArray(array, "Исходный массив");
+string[] result = NewArray(array, Counter(array));
+PrintArray(result, "Получившийся массив");
